a7b9fa387ba74542fd0470bc4b373dea81171a94
gilmujjang <alsanrlf@naver.com>
2021-11-11 18:01:33 +0900

    렌더링 오류 수정

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -56,6 +56,69 @@ const Chart = () => {
   const [intervals, setIntervals] = useState('');
   const [len, setLen] = useState(100);
 
+  function Unix_timestamp(t){
+    const date = new Date(t);
+    const year = date.getFullYear();
+    const month = "0" + (date.getMonth()+1);
+    const day = "0" + date.getDate();
+    const hour = "0" + date.getHours();
+    return year + "-" + month.substr(-2) + "-" + day.substr(-2) + " " + hour.substr(-2);
+  }
+
+  async function candle_price(coin, intervals, length){
+    const api_url = "https://api.bithumb.com/public/candlestick/"+coin+"_KRW/"+intervals;
+    const response = new Promise((resolve, reject) => {
+      request({
+        uri:api_url,
+      },(err, res, result) => {
+        if (err) reject(err);
+        if (res.statusCode !== 200) {
+          reject('Invalid status code <' + response.statusCode + '>');
+        }
+        resolve(JSON.parse(result));
+        }
+      )
+    })
+    const response_json = await response;
+    const n = response_json["data"].length-1
+    let coin_pices_list = [];
+    let coin_date_list = [];
+    const initPrice = response_json["data"][n-length][2];
+    for(let i=length; i>0; i--){
+      const data = response_json["data"][n-i];
+      const time = Unix_timestamp(Number(data[0]));
+      coin_pices_list.push((Number(data[2]))/initPrice).toFixed(1);
+      coin_date_list.push(time);
+    }
+    const reObj = {price : coin_pices_list, date : coin_date_list}
+    return reObj;
+  }
+
+  useEffect(() => {
+    async function DataRenew(){
+      dbService.collection("balance").orderBy("time", "desc").limit(len).get().then(snapshot  => {
+        let data = [];
+        const snapshotReverse = snapshot.docs.reverse();
+        const initAsset = snapshotReverse[0].data().btc +snapshotReverse[0].data().eth+snapshotReverse[0].data().bnb+snapshotReverse[0].data().cash
+        snapshotReverse.map(doc => {
+          const totalMoney = ((doc.data().btc +  doc.data().eth+ doc.data().bnb+ doc.data().cash)/initAsset).toFixed(2);
+          data.push(totalMoney);
+        })
+        setAssets(data)
+      })
+      const btcObj = await candle_price('BTC',intervals,len);
+      const ethObj = await candle_price('ETH',intervals,len);
+      const bnbObj = await candle_price('BNB',intervals,len);
+      const composePrice = btcObj.price.map((x,y) => (Number((x + ethObj.price[y] + bnbObj.price[y])/3).toFixed(2)));
+      setComposePrice(composePrice)
+      setBtcPrice(btcObj.price);
+      setEthPrice(ethObj.price);
+      setBnbPrice(bnbObj.price);
+      setDate(btcObj.date)
+    }
+    DataRenew();
+  },[intervals, len])
+
   const gilmuindex ={
     label: 'Gilmu',
     data: assets,
@@ -138,68 +201,10 @@ const Chart = () => {
       datasetSample.push(bnbindex)
     }
     setDataset(datasetSample);
-  }, [showgilmu,showcomposite,showbtc,showeth,showbnb])
+  }, [showgilmu,showcomposite,showbtc,showeth,showbnb, btcPrice, assets])
 
-  useEffect(() => {
-    async function DataRenew(){
-      dbService.collection("balance").orderBy("time", "desc").limit(len).get().then(snapshot  => {
-        const snapshotReverse = snapshot.docs.reverse();
-        const initAsset = snapshotReverse[0].data().btc +snapshotReverse[0].data().eth+snapshotReverse[0].data().bnb+snapshotReverse[0].data().cash
-        snapshotReverse.map(doc => {
-          const totalMoney = ((doc.data().btc +  doc.data().eth+ doc.data().bnb+ doc.data().cash)/initAsset).toFixed(2);
-          setAssets(assets => [...assets, totalMoney])
-        })
-      })
-      const btcObj = await candle_price('BTC',intervals,len);
-      const ethObj = await candle_price('ETH',intervals,len);
-      const bnbObj = await candle_price('BNB',intervals,len);
-      const composePrice = btcObj.price.map((x,y) => (Number((x + ethObj.price[y] + bnbObj.price[y])/3).toFixed(2)));
-      setComposePrice(composePrice)
-      setBtcPrice(btcObj.price);
-      setEthPrice(ethObj.price);
-      setBnbPrice(bnbObj.price);
-      setDate(btcObj.date)
-    }
-    DataRenew();
-  },[intervals, len])
-
-  function Unix_timestamp(t){
-    const date = new Date(t);
-    const year = date.getFullYear();
-    const month = "0" + (date.getMonth()+1);
-    const day = "0" + date.getDate();
-    const hour = "0" + date.getHours();
-    return year + "-" + month.substr(-2) + "-" + day.substr(-2) + " " + hour.substr(-2);
-  }
-
-  async function candle_price(coin, intervals, length){
-    const api_url = "https://api.bithumb.com/public/candlestick/"+coin+"_KRW/"+intervals;
-    const response = new Promise((resolve, reject) => {
-      request({
-        uri:api_url,
-      },(err, res, result) => {
-        if (err) reject(err);
-        if (res.statusCode !== 200) {
-          reject('Invalid status code <' + response.statusCode + '>');
-        }
-        resolve(JSON.parse(result));
-        }
-      )
-    })
-    const response_json = await response;
-    const n = response_json["data"].length-1
-    let coin_pices_list = [];
-    let coin_date_list = [];
-    const initPrice = response_json["data"][n-length][2];
-    for(let i=length; i>0; i--){
-      const data = response_json["data"][n-i];
-      const time = Unix_timestamp(Number(data[0]));
-      coin_pices_list.push((Number(data[2]))/initPrice).toFixed(1);
-      coin_date_list.push(time);
-    }
-    const reObj = {price : coin_pices_list, date : coin_date_list}
-    return reObj;
-  }
+  console.log(dataset);
+  console.log(assets)
 
   return(
       <WelecomeView className="chart">
