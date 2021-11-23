600c57703b11dc3c8d338157f2f57e492e9306b0
gilmujjang <alsanrlf@naver.com>
2021-11-23 17:17:24 +0900

    보유현황 파이차트 생성

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,34 +4,15 @@ import QuantLogic from './QuantLogic';
 import CoinChart from './CoinChart';
 import GilmuIndex from './GilmuIndex';
 import MyAsset from './MyAsset';
-import styled from 'styled-components';
-
-// const BackgrondOdd = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   background: linear-gradient(125deg, #ECFCFF 0%, #ECFCFF 40%, #B2FCFF calc(40% + 1px), #B2FCFF 60%, #5EDFFF calc(60% + 1px), #5EDFFF 72%, #4facfe calc(72% + 1px), #4facfe 100%); 
-//   `;
-
-// const BackgrondEven = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   background: linear-gradient(55deg, #ECFCFF 0%, #ECFCFF 40%, #B2FCFF calc(40% + 1px), #B2FCFF 60%, #5EDFFF calc(60% + 1px), #5EDFFF 72%, #4facfe calc(72% + 1px), #4facfe 100%); 
-//   `;
 
 const Main = () => {
   return(
     <div>
       <GilmuIndex/>
-      {/* <BackgrondOdd> */}
       <Welcome/>
-      {/* </BackgrondOdd>
-      <BackgrondEven> */}
       <QuantLogic/>
-      {/* </BackgrondOdd>
-      <BackgrondEven> */}
       <CoinChart/>
       <MyAsset/>
-      {/* </BackgrondEven> */}
     </div>
   )
 }
--- src/components/MyAsset.js
+++ src/components/MyAsset.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import { dbService } from '../fbase';
-import { Line } from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
 
 const WelecomeView = styled.div`
 color: black;
@@ -21,45 +21,48 @@ const Title = styled.p`
 `;
 
 const ChartImage = styled.div`
-  width: 48rem;
-  height: 24rem;
+  margin: auto;
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 36rem;
   padding: 1rem;
-  background-color: #eee;
-  border-radius: 0.8rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 `;
 
 const MyAsset = () => {
-  const [assets, setAssets] = useState([]);
-  const [date, setDate] = useState([]);
+  const [assets, setAssets] = useState({cash: 1000000, btc: 0, eth: 0, bnb: 0});
 
   useEffect(() => {
-    dbService.collection("balance").orderBy("time", "desc").limit(50).get().then(snapshot  => {
-      const snapshotReverse = snapshot.docs.reverse();
-      snapshotReverse.map(doc => {
-        const totalMoney = doc.data().btc +  doc.data().eth+ doc.data().bnb+ doc.data().cash;
-        setAssets(assets => [...assets, totalMoney])
-        setDate(date => [...date,doc.data().time.substr(0,8)])
+    dbService.collection("balance").orderBy("time", "desc").limit(1).get().then(snapshot  => {
+      snapshot.docs.map(doc => {
+        const {cash, btc, eth, bnb} = doc.data();
+        setAssets({cash: cash, btc: btc, eth: eth, bnb: bnb});
       })
     })
   },[])
 
-  const dataset = [{
-      label: 'Daily Assets',
-      data: assets,
-      fill: true,
-      borderColor: 'rgb(100, 100, 100)',
-      tension: 0.1
-    }
-  ]
+  const data = {
+      labels: ['현금', '비트코인', '이더리움', '바이낸스코인'],
+      datasets: [{
+        label: '보유 현황',
+        data: [assets.cash, assets.btc, assets.eth, assets.bnb],
+        backgroundColor: [
+          '#FF6384',
+          '#ffd900',
+          '#9c9c9c',
+          '#00aa00'
+        ],
+        hoverOffset: 4
+      }]
+    };
   return(
       <WelecomeView className="chart">
-        <Title>차트</Title>
+        <Title>보유현황</Title>
         <ChartImage>
-          <Line
-            data={{
-              labels: date,
-              datasets: dataset
-            }}
+          <Doughnut
+            data={data} 
             style={{ height: "100%", width: "100%" }}
             options={{ responsive: true},{elements: { point: { radius: 0 } }}}
           />
