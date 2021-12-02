38afcb171fa7931a01065dd286bf75f17e12948b
gilmujjang <alsanrlf@naver.com>
2021-12-02 22:55:55 +0900

    파일 날려먹어서 apikey 입력하는데 env가 안되서 바꿈

--- src/fbase.js
+++ src/fbase.js
@@ -2,15 +2,16 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import apikey from './apikey.js';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: apikey.REACT_APP_FIREBASE_API_KEY,
+  authDomain: apikey.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: apikey.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: apikey.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: apikey.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: apikey.REACT_APP_FIREBASE_APP_ID,
+  measurementId: apikey.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
